b683581336cc6e018f5ecc5afb36bb481d289140
r7kamura <r7kamura@gmail.com>
2020-09-11 23:18:42 +0900

    bundle gem activerecord-enum_translation

---
+++ lib/activerecord/enum_translation.rb
@@ -0,0 +1,8 @@
+require "activerecord/enum_translation/version"
+
+module Activerecord
+  module EnumTranslation
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/enum_translation/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module EnumTranslation
+    VERSION = "0.1.0"
+  end
+end
